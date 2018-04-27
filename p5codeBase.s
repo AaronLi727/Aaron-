526be08743acaacb3cf48f1ee91da6647954219c
A-a-ron <32628998+AaronLi727@users.noreply.github.com>
2018-04-27 02:10:58 -0700

    Add files via upload

--- p5codeBase.s
+++ p5codeBase.s
@@ -1,70 +1,100 @@
 .data
-  expVal23:    .asciiz  "Expected Value : 23  Your Value : "
-  expVal21:    .asciiz  "Expected Value : 21  Your Value : "
-  endl:        .asciiz  "\n"
+expVal123: .asciiz "Expected value:23 Your value: "
+expVal121: .asciiz "Expected value:21 Your value: "
+endl: .asciiz "\n"
 
 .text
 
-# #
+##
 # int getDigit(int number);
 # List Used Registers Here:
-#
-# #
-getDigit:
-
-
-
-##
-# int sumOfDoubleEvenPlace(int number);
-# List Used Registers Here:
-# sum --> $s0
-# digit --> $s1
+# $t0 = sum
+# $t3 = number
+# $t4 = number ( %//) 10
+# $a1 = 10
 # 
 ##
+
+getDigit:
+mul $t3,$t2,2	# sum * 2  ( digit * 2 )
+bge $t3,10,else	# ( number !< 10)
+add $t0,$t0,$t3	# sum = number
+j return
+else:
+div $t3,$a1	# number % 10
+mfhi $t4	# Hi 
+add $t0,$t0,$t4	# sum += number % 10
+div $t3,$a1	# number / 10
+mflo $t4	# Lo 
+add $t0,$t0,$t4	# sum += number % 10 + number / 10
+j return
+
+##
+# int sumOfDoubleEvenPlace (int number);
+# sum --> $s0 
+# digit --> $s1
+#
+##
+
 sumOfDoubleEvenPlace:
+li $t0,0	# $t0 = sum = 0
+li $a1,10	# $a1 = 10
+li $a2,100 	# $a2 = 100
+div $t1,$a1	# number = number/ 10
+mflo $t1	# Lo 
+while:
+div $t1,$a1 # number = number / 10
+mfhi $t2	# Hi 
+j getDigit	# returns sum
+return:
+div $t1,$a2	# number = number / 100
+mflo $t1	# Lo 
+bgt $t1,0,while # number !> 0
+jr $ra
+  
+
+
+
 
 main:
-  li $s0, 89744563  # int test1 = 89744563;
-  li $s1, 98756421  # int test2 = 98756421;
-  li $s2, 0         # int result1 = 0;
-  li $s3, 0         # int result2 = 0;
+li $s0,89744563 # int test1 = 89744563;
+li $s1,98756421 # int test2 = 98756421;
+li $s2,0	# int result1 = 0;
+li $s3,0	# int result2 = 0;
+
+# code for first function call
+
+move $t1,$s0 
+jal sumOfDoubleEvenPlace
+move $s2,$t0
+
+li $v0,4
+la $a0,expVal123
+syscall
+
+li $v0,1
+move $a0,$s2
+syscall
+
+li $v0,4
+la $a0,endl
+syscall
+
+# code for second function call
+
+move $t1,$s1
+jal sumOfDoubleEvenPlace
+move $s3,$t0
+
+li $v0,4
+la $a0,expVal121
+syscall
+  
+li $v0,1
+move $a0,$s3
+syscall
 
 
-  # code for first function call
 
-  add $a0, $0, $s0
-  jal sumOfDoubleEvenPlace
-  add $s2, $0, $v0 
-
-  la   $a0, expVal23     
-  addi $v0, $0, 4     
-  syscall             
-
-  move $a0, $s2       
-  addi $v0, $0, 1     
-  syscall             
-
-  la   $a0, endl      
-  addi $v0, $0, 4     
-  syscall
-
-   # code for first function call
-
-  add $a0, $0, $s1
-  jal sumOfDoubleEvenPlace
-  add $s3, $0, $v0 
-
-  la   $a0, expVal21     
-  addi $v0, $0, 4     
-  syscall             
-
-  move $a0, $s3       
-  addi $v0, $0, 1     
-  syscall             
-
-  la   $a0, endl      
-  addi $v0, $0, 4     
-  syscall
-
-  li $v0, 10
-  syscall
+li $v0,10
+syscall
